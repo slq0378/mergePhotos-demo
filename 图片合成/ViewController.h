ed03da090b91f4e8ef318e4c37eb8d2f66cc7f40
slq0378 <slq0378@gmial.com>
2016-03-05 17:44:04 +0800

    Initial Commit

---
+++ 图片合成/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片合成
+//
+//  Created by Christian on 16/3/5.
+//  Copyright © 2016年 slq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 图片合成/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  图片合成
+//
+//  Created by Christian on 16/3/5.
+//  Copyright © 2016年 slq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
